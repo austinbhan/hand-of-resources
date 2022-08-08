0a9fff785178d5aedc8ce121045a2fc48f337984
austinbhan <austinbhan@outlook.com>
2022-08-07 22:44:42 -0700

    Create vegetable talb,e and relevant model, controller, test files

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS vegetables;
+
+CREATE TABLE vegetables (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    color VARCHAR NOT NULL
+);
+
+INSERT INTO 
+    vegetables (name, color)
+VALUES
+('Potato', 'Brown'),
+('Bell Pepper','Red'),
+('Carrot','Orange'),
+('Onion','Yellow'),
+('Kale','Green');
