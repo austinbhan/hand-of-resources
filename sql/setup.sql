993e587371dcfc8821063d9f1686056188ce73a0
austinbhan <austinbhan@outlook.com>
2022-08-15 11:06:08 -0700

    Dictators table, controller, model, and test files

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS vegetables;
+DROP TABLE IF EXISTS dictators;
 
 CREATE TABLE vegetables (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     color VARCHAR NOT NULL
+);
+CREATE TABLE dictators (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    country VARCHAR NOT NULL
 );
 
 INSERT INTO 
@@ -16,3 +22,12 @@ VALUES
 ('Carrot','Orange'),
 ('Onion','Yellow'),
 ('Kale','Green');
+
+INSERT INTO
+    dictators (name, country)
+VALUES 
+('Josef Tito', 'Yugoslavia'),
+('Park Chung He', 'South Korea'),
+('Mobutu Seko','Zaire'),
+('Francisco Franco','Spain'),
+('Manuel Noreiga','Panama');
