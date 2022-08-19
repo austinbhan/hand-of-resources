78fe10200f6e50ac38001376b180c674ce5a2b05
austinbhan <austinbhan@outlook.com>
2022-08-19 12:25:09 -0700

    Created games table, app.js, blank controller/model/test

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS vegetables;
 DROP TABLE IF EXISTS dictators;
 DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS games;
 
 CREATE TABLE vegetables (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -18,6 +19,12 @@ CREATE TABLE cities (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     country VARCHAR NOT NULL
+);
+CREATE TABLE games (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    developer VARCHAR NOT NULL,
+    genre VARCHAR NOT NULL
 );
 
 INSERT INTO 
@@ -46,3 +53,12 @@ VALUES
 ('Lisbon', 'Portugal'),
 ('Cape Town', 'South Africa'),
 ('Sarajevo', 'Bosnia and Herzegovina');
+
+INSERT INTO 
+    games (title, developer, genre)
+VALUES 
+('X-Com', 'Firaxis', 'Strategy'),
+('Frostpunk', '11-bit', 'City building'),
+('Call of Duty', 'Infinity Ward', 'FPS'),
+('Microsoft Flight Simulator', 'Asobo', 'Simulation'),
+('Super Meat Boy', 'Team Meat', 'Platformer');
